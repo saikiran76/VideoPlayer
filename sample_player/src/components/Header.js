e317347e72415f59fd8f82c039bb822bcf539631
kiran76 <ksknew76@gmail.com>
2024-05-17 19:00:51 +0530

    integrated tailwind

--- sample_player/src/components/Header.js
+++ sample_player/src/components/Header.js
@@ -1,8 +1,8 @@
 const Header = () =>{
     return(
-        <>
-        <h1>Video Player with Notes</h1>
-        </>
+        <div>
+            <h1 className="font-inter m-6 text-2xl">Video Player with Notes</h1>
+        </div>
 
     )
 }
